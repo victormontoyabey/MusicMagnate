d3b9a02ff27a9b7b3c07c51c4dae56aee3e939b0
Victor Montoya <victormontoyabey@gmail.com>
2020-08-24 23:09:09 -0700

    Proud of the styling on this one - very modern and fun. The functionality works perfectly, as well. Fixed the problem that was preventing us from properly linking Javascript file to the mark-up page.

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const colors = [
+    "#264653",
+    "#2a9d8f",
+    "#f4a261",
+    "#e76f51",
+    "#2a9d8f",
+    "#264653",
+  ];
+
+  // Start the sound
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function () {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+
+      createBubbles(index);
+    });
+  });
+
+  // Create a function that generates bubbles
+  const createBubbles = (index) => {
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = "jump 1s ease";
+  };
+});
